c71fdc82c9f8b1b8b27b912d0042f71432da3e05
Fariha Hasan <130080229+Fariha223@users.noreply.github.com>
2023-04-07 14:19:13 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LoginPage from "./view/LoginPage";  
+import SignUp from "./view/Signup";
 function App(){
     return(
      <div className= "app">
         <BrowserRouter>
           <Routes>
             <Route path= "/login" element={<LoginPage/>} />
-
+            <Route path= "/signup" element={<SignUp/>} />
           </Routes>
         </BrowserRouter>
 
